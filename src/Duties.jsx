375546d89475284ef16c147d41ba079de1c6d5cf
luiznaiper <luiz_naiper@hotmail.com>
2023-03-30 06:38:13 -0300

    :construction: Add unique ID with uuidv4 library

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,7 +1,20 @@
-const Duties = ({ duties }) => {
-  console.log(duties)
+import { FaAngleDoubleRight } from 'react-icons/fa'
+import { v4 as uuidv4 } from 'uuid'
 
-  return <div>Duties</div>
+const Duties = ({ duties }) => {
+  return (
+    <div>
+      {duties.map((duty) => {
+        const id = uuidv4()
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Duties
