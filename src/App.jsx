7a6f1c03e001f30c233325949086ac836b6de4a4
luiznaiper <luiz_naiper@hotmail.com>
2023-03-30 06:18:35 -0300

    :construction: Fetch jobs and add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from 'react'
+
+const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setJobs(data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    )
+  }
+
   return (
     <div className="App">
       <h1>Hola</h1>
